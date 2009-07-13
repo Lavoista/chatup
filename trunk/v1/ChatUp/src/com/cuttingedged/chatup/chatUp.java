7230efd1bb71c810782abba1c1b4727c2a8e6bd1
keybreen <keybreen@ac63651e-6fc2-11de-b0c7-2b5f3fb92cbb>
2009-07-13 18:09:21 +0000

    the first commit by keybreen

---
+++ trunk/v1/ChatUp/src/com/cuttingedged/chatup/chatUp.java
@@ -0,0 +1,13 @@
+package com.cuttingedged.chatup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class chatUp extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
